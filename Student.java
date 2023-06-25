0845212f95b34836ab6e0d4013be7381cb88f42c
aryansriram <116143036+aryansriram@users.noreply.github.com>
2023-06-25 19:35:43 +0530

    Add files via upload

---
+++ Student.java
@@ -0,0 +1,16 @@
+class Student
+{
+int id=1122;
+String name="Amith";
+}
+//Creating another class TestStudent1 which contains the main method
+class Student123
+{
+public static void main(String args[])
+{
+Student s1=new Student();
+System.out.println(s1.id);
+System.out.println(" \n");
+System.out.println(s1.name);
+}
+}
